e8c328caedd66f9871b7f5fd39c82a3dc24799e9
Filip <filip.lagerlov@gmail.com>
2013-04-27 14:13:34 +0200

    Create user started

--- sim.js
+++ sim.js
@@ -1,7 +1,7 @@
 var express = require('express'),stylus = require('stylus'),nib = require('nib')
 var dirname = "/home/fille/siv/"
 var mysql =  require('mysql')
-
+var crypto = require('crypto')
 var connection = mysql.createConnection({host:'localhost',user:'root',password:'root',database:"myinter"})
 
 
@@ -30,17 +30,35 @@ app.post('/login',function(req,res){
 	
 	var user = req.body.user;
 	var pass = req.body.pass;
-		
-	connection.connect();	
-	connection.query("select * from users where user='"+ user +"'",function(err,rows,fields){
-		
-		
-		console.log(rows);
-	 	res.render('index',{title:'Home','status':"found"});
-		
-		
+	realpass = getHasha(pass)
+	connection.query("select * from Users where username='"+ user +"' and '"+ realpass + "'" ,function(err,rows,fields){
+		rows.forEach(function(obj) {
+				if(obj.username == user) {
+					console.log("found")
+					return false;
+				}
+		})		
+		res.render('index',{title:'Home','status':"found"});
 	})
 });
+
+var getHasha = function(string) {
+	var sha = crypto.createHash('sha1');
+	sha.update(string+ "salt");
+ 	var d =	sha.digest('hex');
+	return d;	
+}
+app.post('/createUser',function(req,res) {
+	
+	var user = req.body.username;
+	var pass = req.body.pass;
+	var realpass = getHasha(pass)
+	sql ="insert into Users(username,password) Values('"+ user +"','"+ realpass +"')"	
+	connection.query(sql,function(err,row,fields) {
+		console.log(user)
+	res.end(user);	
+  })
+})
 
 
 app.listen(3000)
