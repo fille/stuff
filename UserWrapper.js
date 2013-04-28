78a488dca984319644aebd3f530effe8b28d53ec
Filip <filip.lagerlov@gmail.com>
2013-04-28 15:03:17 +0200

    add

--- UserWrapper.js
+++ UserWrapper.js
@@ -8,11 +8,16 @@ var  wrapper = function(conn) {
 
    var self = this;
 	self.connection = conn;
-	
-	self.GetUserWithPassword= function(username,password) {
-		self.connection.query("select * from Users where username='"+ username +"' and '"+ password + "'" ,function(err,rows,fields){
-				console.log(rows);
+	self.GetUserWithPassword= function(username,password,callback) {
+
+		self.connection.query("select * from Users where username='"+ username +"' and password='"+ password + "'" ,function(err,rows,fields){
+				callback(rows);
 		});
 	}
-
+	self.Insert= function(username,password){
+	
+		sql ="insert into Users(username,password) Values('"+ username +"','"+ password +"')"	
+		self.connection.query(sql,function(err,row,fields) {
+		})
+	}
 }
--- sim.js
+++ sim.js
@@ -4,6 +4,7 @@ var mysql =  require('mysql')
 var crypto = require('crypto')
 var connection = mysql.createConnection({host:'localhost',user:'root',password:'root',database:"myinter"})
 
+var userwrapper =  require('./UserWrapper').UserWrapper(connection);
 
 var compiler = function(str,path) {	
 	return  stylus.set('filename',path).use(nib())
@@ -26,21 +27,25 @@ app.get('/',function(req,res) {
 		res.render('index',{title:'Home'});
 });
 
-app.post('/login',function(req,res){
-	
+app.post('/login',function(req,res,next){
 	var user = req.body.user;
 	var pass = req.body.pass;
-	realpass = getHasha(pass)
-	connection.query("select * from Users where username='"+ user +"' and '"+ realpass + "'" ,function(err,rows,fields){
-		rows.forEach(function(obj) {
+	var realpass = getHasha(pass)
+	userwrapper.GetUserWithPassword(user,realpass,function(rows) {
+	
+	rows.forEach(function(obj) {
 				if(obj.username == user) {
-					console.log("found")
-					return false;
+					
+					if(typeof req.cookies['connection.sid'] != 'undefiend') {
+						 console.log(req.cookies['connect.sid']);
+					}
+					next();
 				}
-		})		
-		res.render('index',{title:'Home','status':"found"});
+		})
+		
+	res.render('index',{title:'Home','status':"found"});
 	})
-});
+	})
 
 var getHasha = function(string) {
 	var sha = crypto.createHash('sha1');
@@ -49,16 +54,12 @@ var getHasha = function(string) {
 	return d;	
 }
 app.post('/createUser',function(req,res) {
-	
-	var user = req.body.username;
+	var user = req.body.name;
 	var pass = req.body.pass;
-	var realpass = getHasha(pass)
-	sql ="insert into Users(username,password) Values('"+ user +"','"+ realpass +"')"	
-	connection.query(sql,function(err,row,fields) {
-		console.log(user)
-	res.end(user);	
+	var realpass = getHasha(pass);
+	var rows =userwrapper.Insert(user,realpass)
+	 res.end(user);
   })
-})
 
 
 app.listen(3000)
